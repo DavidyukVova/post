b8cff09384849de167fd634d1cead85bc9a97d67
Vova <vovkadavidyud@gmail.com>
2017-12-18 00:33:00 +0200

    five_commit

--- docs/index.php
+++ docs/index.php
@@ -5,14 +5,30 @@
 	<title>Document</title>
   </head>
 <body>
-	<form action= "post.php" method= "POST"> 
- 
-<p>Имя: </p><p> <input type= "text" name= "name"> </p> 
- 
-<p>E-mail: </p><p> <input type= "text" name= "email"></p> 
- 
-<p>Сообщение: </p><p> <textarea rows= "10" cols= "45" name= "message"></textarea></p> 
- 
-<input type= "submit" value= "Отправить"> 
+<?php
+//проверяем, существуют ли переменные в массиве POST
+if(!isset($_POST['fio']) and !isset($_POST['email'])){
+?> <form action="send.php" method="post">
+<input type="text" name="fio" placeholder="Укажите ФИО" required>
+<input type="text" name="email" placeholder="Укажите e-mail" required>
+<input type="submit" value="Отправить">
+</form> <?php
+} else {
+//показываем форму
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
+echo "Сообщение успешно отправлено";
+} else {
+echo "При отправке сообщения возникли ошибки";
+}
+}
+?>
 </body>
 </html>
